2615f254c7a76141f818bf169f71b5aed4649a04
anslava00 <anslava00@mail.ru>
2022-06-09 20:44:46 +1000

    add excel load

--- resources/views/web/create_page.blade.php
+++ resources/views/web/create_page.blade.php
@@ -23,6 +23,7 @@
                 <div class="select" name="splash2">
                     <div name="forHeader">Дисциплины: </div>
                     <select name = "chooseRpd">
+                        <option value = 1>@isset($rpd) {{$rpd->discipline}}@endisset</option>
                         <input type="submit" name="EditFooterBTN" value="get">
                     </select>
                 </div>
@@ -42,7 +43,9 @@
                 <form method="post" action="" enctype="multipart/form-data">
                     @csrf
                     <input type="file" name="image">
-                    <button type="submit" >Отправить</button>
+                    <button type="submit" class="button11" name = 'EditFooterBTN' value = 'send'>Отправить</button>
+                    <input class="input4" name="sheet" @isset($sheet) value="{{$sheet}}" @endisset  type="text" placeholder="название листа">
+                    <input class="input5" name="rows" @isset($row) value="{{$row}}"@endisset  type="text" placeholder="номер строки">
                 </form>
             </li>
         </tr>
